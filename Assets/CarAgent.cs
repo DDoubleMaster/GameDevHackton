4d73566c1693e0cab06eef02a1d04bce0e7a948c
DoubleMaster <130893087+DDoubleMaster@users.noreply.github.com>
2024-10-20 22:08:20 +0500

    Update Agent Script

--- Assets/CarAgent.cs
+++ Assets/CarAgent.cs
@@ -7,72 +7,56 @@ public class CarAgent : Agent
 {
 	[SerializeField] float speed = 5;
 	[SerializeField] float rotateSpeed = 120;
-	public Transform target;
-	private Vector3 mainPos;
-	private Vector3 targetPos;
+	[SerializeField] float radius = 5;
 
-	public override void Initialize()
-	{
-		mainPos = transform.position;
-	}
+	public Transform target;
 
 	public override void OnEpisodeBegin()
 	{
-		float radius = 5;
-		Debug.Log("New Episode");
-		// —брос позиции агента
-		if (transform.localPosition.y < 0)
+		if (transform.position.y < 0)
 		{
-			transform.position = mainPos;
-			transform.localRotation = Quaternion.identity;
-		}
-		else
-		{
-			transform.localPosition = new Vector3(Random.Range(-radius, radius), 0.5f, Random.Range(-radius, radius));
+			transform.localPosition = Vector3.zero;
 			transform.localRotation = Quaternion.identity;
 		}
 
-		// —брос позиции цели
-		target.localPosition = new Vector3(Random.Range(-radius, radius), 0.5f, Random.Range(-radius, radius));
+		target.localPosition = new Vector3(Random.Range(-radius, radius), 0, Random.Range(-radius, radius));
 	}
 
 	public override void CollectObservations(VectorSensor sensor)
 	{
-		sensor.AddObservation(transform.localPosition);
-		sensor.AddObservation(target.localPosition);
-		sensor.AddObservation(transform.localRotation);
+		sensor.AddObservation(transform.position);
+		sensor.AddObservation(transform.rotation);
+		sensor.AddObservation(target.position);
 	}
 
 	public override void OnActionReceived(ActionBuffers actions)
 	{
-		float move = actions.ContinuousActions[0];
-		float rotate = actions.ContinuousActions[1];
-		transform.Translate(Vector3.forward * Time.deltaTime * move * speed);
+		float forward = actions.ContinuousActions[0];
+		float right = actions.ContinuousActions[1];
 
-		if (move != 0)
+		transform.Translate(Vector3.forward * Time.deltaTime * forward * speed);
+
+		if (forward != 0)
+			transform.Rotate(Vector3.up * Time.deltaTime * forward * right * rotateSpeed);
+
+		float distance = Vector3.Distance(transform.position, target.position);
+		if (distance < 2.5f)
 		{
-			transform.Rotate(Vector3.up * Time.deltaTime * rotate * rotateSpeed);
-		}
+			SetReward(1f + (forward/10));
+			EndEpisode();
+		} 
 
-		float distance = Vector3.Distance(transform.localPosition, target.localPosition);
-
-		if (distance < 1f)
+		if (transform.position.y < 0)
 		{
-			SetReward(2.0f);
+			SetReward(-2f);
 			EndEpisode();
 		}
-
-		if (transform.localPosition.y < 0)
-		{
-			SetReward(-1.0f);
-			EndEpisode();
-		}
-	}
+    }
 
 	public override void Heuristic(in ActionBuffers actionsOut)
 	{
-		ActionSegment<float> continuousAction = actionsOut.ContinuousActions;
-		continuousAction[0] = Input.GetAxis("Vertical");
-		continuousAction[1] = Input.GetAxis("Horizontal");
+		ActionSegment<float> ao = actionsOut.ContinuousActions;
+		ao[0] = Input.GetAxis("Vertical");
+		ao[1] = Input.GetAxis("Horizontal");
 	}
 }
